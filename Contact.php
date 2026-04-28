40b43cff9648da5468fe9c9067460ae45f63718b
AlexCh79 <chanteloup.a@laposte.net>
2026-04-28 18:59:00 +0200

    Création des classes

---
+++ Contact.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+class Contact
+{
+    // ID
+    private ?int $id = null;
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    // Name
+    private ?string $name = null;
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    // Email
+    private ?string $email = null;
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    // N° Tel
+    private ?string $phone = null;
+
+    public function setPhone(string $phone):void
+    {
+        $this->phone = $phone;
+    }
+
+    public function getPhone(): string
+    {
+        return $this->phone;
+    }
+
+    public function __toString(): string
+    {
+        return "{$this->id}, {$this->name}, {$this->email}, {$this->phone}";
+    }
+}
--- ContactManager.php
+++ ContactManager.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 require_once 'DBConnect.php';
+require_once 'Contact.php';
 
 class ContactManager extends DBConnect
 {
@@ -11,9 +12,24 @@ class ContactManager extends DBConnect
         $db = parent::getPDO();
         $db = $db->prepare('SELECT * FROM contact');
         $db->execute();
-        return $db->fetchAll();
+        $db = $db->fetchAll();
+        
+        $contacts = [];
+
+        foreach ($db as $ligne){
+            $contact = new Contact();
+            $contact->setId($ligne['id']);
+            $contact->setName($ligne['name']);
+            $contact->setEmail($ligne['email']);
+            $contact->setPhone($ligne['phone_number']);
+            $contacts[] = $contact;
+        }
+        return $contacts;
     }
 }
+$liste = new ContactManager;
+$liste = $liste->findAll();
 
-$contact = new ContactManager;
-var_dump($contact->findAll());
+foreach ($liste as $c){
+    echo $c ."<br>";
+}
