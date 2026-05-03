b7eb96789860aa79117a1a1411e456bb65e558a8
AlexCh79 <chanteloup.a@laposte.net>
2026-05-03 16:39:56 +0200

    Modification de la connexion à la base de données et masquage du fichier Config

--- DBConnect.php
+++ DBConnect.php
@@ -2,11 +2,13 @@
 
 declare(strict_types=1);
 
+require_once 'Config.php';
+
 // Connexion à la base de données
 class DBConnect
 {
     public function getPDO()
     {
-        return new PDO ('mysql:host=localhost;dbname=gestion_contacts;charset=utf8','root','root');
+        return new PDO ('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8', DB_USER, DB_PASSWORD);
     }
 }
