ad02cc6a4840b4b024c7ee414b9e6b99fe57d879
AlexCh79 <chanteloup.a@laposte.net>
2026-05-11 21:21:39 +0200

    Correction syntaxe dans fichier main

--- main.php
+++ main.php
@@ -13,17 +13,17 @@ while ($isOn) {
     if ($line === 'list') {
         $command->listContacts();
     } elseif (preg_match('/^detail\s+(\d+)$/', $line, $matches)) {
-        $id = (int)$matches[1];
+        $id = (int) $matches[1];
         $command->detailsContact($id);
     } elseif (preg_match('/^create\s+(.+)$/', $line, $matches)) {
         $data = $matches[1];
         $contact = array_map('trim', explode(',',$data));
         $command->createContact($contact);
     } elseif (preg_match('/^delete\s+(\d+)$/', $line, $matches)) {
-        $id = (int)$matches[1];
+        $id = (int) $matches[1];
         $command->deleteContact($id);
     } elseif (preg_match('/^modify\s+(\d+),\s+(.+)$/', $line, $matches)) {
-        $id = (int)$matches[1];
+        $id = (int) $matches[1];
         $data = $matches[2];
         $contact = array_map('trim', explode(',',$data));
         $command->modifyContact($id, $contact);
